c89a56d53d5b10f94d075654fcd98d377f0e637f
Alvaro Ortiz <alvaro.ortiz23@hotmail.com>
2019-06-22 01:32:00 +0000

    React + Fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    
+    this.state={
+      users: [],
+      url: "https://jsonplaceholder.typicode.com/users",
+    }
+  }
+  
+  componentDidMount() {
+    this.cargar(this.state.url);
+  }
+  
+  cargar (url){
+    console.log("Aqui estoy");
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "applitacion/json"
+      }
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({users: data});
+      })
+      .catch();
+  }
+  
+  render (){
+    return (
+      
+      <div className="App">
+       <h1>Listado de usuarios</h1>
+       <ul>
+       
+       {
+         this.state.users.map((item, i) => {
+           return <li key={i}>{item.name}</li>
+         })
+       }
+       
+       </ul>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
